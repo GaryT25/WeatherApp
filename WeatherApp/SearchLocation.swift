135d324dbc467749796096c9fd64c61bf8173711
Gary Thompson <truegarythompson@gmail.com>
2024-02-05 00:13:34 -0500

    Added a bunch of new things and vastly improved the ui. There is no data yet

---
+++ WeatherApp/SearchLocation.swift
@@ -0,0 +1,18 @@
+//
+//  SearchLocation.swift
+//  WeatherApp
+//
+//  Created by Gary Thompson on 2/4/24.
+//
+
+import SwiftUI
+
+struct SearchLocation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchLocation()
+}
